c42d0cc4fc43c6a280737dbe27317e1f755f1f99
hugorrsh10 <hugorrsh10@gmail.com>
2025-12-30 21:23:40 +0000

    conexao back - html previamente estabelecida

--- general_routes.py
+++ general_routes.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-general_router = APIRouter(prefix="/", tags=["/"])
 
+templates = Jinja2Templates(directory="templates")
 
-@general_router("/write_letter")
-async def write_letter():
-    return
+general_router = APIRouter(prefix="/app", tags=["/app"])
+
+@general_router.get("/write_letter")
+async def write_letter(request: Request):
+    return templates.TemplateResponse(
+        request = request,
+        name="write_letter.html",
+        context={"request": request}
+    )
--- main.py
+++ main.py
@@ -4,5 +4,8 @@ app = FastAPI()
 # INICIAR APP: uvicorn main:app --reload
 
 from auth_routes import auth_router
+from general_routes import general_router
+
 
 app.include_router(auth_router)
+app.include_router(general_router)
