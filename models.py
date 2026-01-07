793effc899544bfd69c7d1d2915ba22e2bdaa71a
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2026-01-07 19:13:43 -0300

    incluindo letters_model e pequenos ajustes html

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, DateTime, ForeignKey, Text
 from sqlalchemy.orm import declarative_base
+from enum import Enum
+from datetime import datetime
 
 db = create_engine("sqlite:///database.db")
 
@@ -22,6 +24,23 @@ class User(base):
         
 
 
+class LetterStatus(Enum):
+    DRAFT = "draft"       # Rascunho
+    SENT = "sent"         # Enviada
+    ARCHIVED = "archived" # Arquivada 
+
+class Letter(base):
+    __tablename__ = "letters"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    sender_id = Column("sender_id", ForeignKey("users.id"), nullable=False)
+    recipient_id = Column("recipient_id", ForeignKey("users.id"), nullable = False)
+    content = Column("content", Text, nullable=False)
+    status = Column("status", String, default=LetterStatus.DRAFT, nullable=False)
+    created_at = Column("created_at", DateTime,default=datetime.now, nullable=False)
+    sent_at = Column("sent_at", DateTime,default=None, nullable=True)
+    delivery_at = Column("delivery_at", DateTime ,default=None, nullable=True)
+    is_read = Column(Boolean, default=False)
 
 
 # base.metadata.create_all(db)
