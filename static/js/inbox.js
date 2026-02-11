71b1b21c34a684109dadff5de58fda3783a7ed40
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2026-02-11 20:24:50 -0300

    adicionado logica de filtro na cartas inbox

--- static/js/inbox.js
+++ static/js/inbox.js
@@ -1,36 +1,13 @@
+var globalLetters = [];
+
 document.addEventListener('DOMContentLoaded', async () =>{
 
     try{
         const response = await fetch('/app/letters/inbox')
         const letters = await response.json();
-        const lettersContainer = document.querySelector('#inbox-grid')
-        let htmlContent = '';
+        globalLetters = letters;
+        LettersRender(letters);
 
-
-        letters.forEach(element => {
-            htmlContent += `<div class="envelope-card ${element.read ? "read": null}" onclick=readLetter(event) data-id="${element.uuid}" data-read="${element.read}">
-        
-            <div class="envelope-flap"></div>
-            
-            <div class="envelope-info">
-                <div class="sender-info">
-                    <i class="ph ph-user-circle"></i>
-                    <span>De: ${element.sender_nick}</span>
-                </div>
-                <div class="date-info">
-                    <small>${element.created_at || ''}</small>
-                </div>
-            </div>
-            <div class="envelope-flap"></div>
-            <div class="wax-seal">
-                ${element.read ? '<i class="ph ph-envelope-open"></i>' : '<i class="ph ph-seal-check"></i>'}
-            </div>
-            <div class="letterBody" >${element.content}</div>
-        </div>`
-        });
-        console.log(letters)
-
-        lettersContainer.insertAdjacentHTML('beforeend', htmlContent);
     }
     catch(e) {
         console.log("Error ao carregar cartas",e)
@@ -38,6 +15,51 @@ document.addEventListener('DOMContentLoaded', async () =>{
     }
 
 })
+
+
+function LettersRender(letters, filterIndex = 0){
+
+    const unreadLetters = letters.filter(letter => !letter.read);
+    const readLetters = letters.filter(letter => letter.read);
+    let filteredLetters = [];
+
+    if(filterIndex == 0){
+        filteredLetters = letters;
+    }else if(filterIndex == 1){
+        filteredLetters = unreadLetters;
+    }else{
+        filteredLetters = readLetters;
+    }
+
+    const lettersContainer = document.querySelector('#inbox-grid')
+    lettersContainer.innerHTML = '';
+    let htmlContent = '';
+
+    filteredLetters.forEach(element => {
+        htmlContent += `<div class="envelope-card ${element.read ? "read": null}" onclick=readLetter(event) data-id="${element.uuid}" data-read="${element.read}">
+    
+        <div class="envelope-flap"></div>
+        
+        <div class="envelope-info">
+            <div class="sender-info">
+                <i class="ph ph-user-circle"></i>
+                <span>De: ${element.sender_nick}</span>
+            </div>
+            <div class="date-info">
+                <small>${element.created_at || ''}</small>
+            </div>
+        </div>
+        <div class="envelope-flap"></div>
+        <div class="wax-seal">
+            ${element.read ? '<i class="ph ph-envelope-open"></i>' : '<i class="ph ph-seal-check"></i>'}
+        </div>
+        <div class="letterBody" >${element.content}</div>
+    </div>`
+    });
+
+    lettersContainer.insertAdjacentHTML('beforeend', htmlContent);
+
+}
 
 async function readLetter(event){
     
@@ -73,6 +95,9 @@ async function readLetter(event){
         if(response.ok){
             if(maxSeal){
                 maxSeal.classList.replace("ph-seal-check", "ph-envelope-open");
+                clickedLetter.setAttribute("data-read", "true");
+                globalLetters.find(letter => letter.uuid == letterId).read = true;
+                console.log(globalLetters);
             }
         }else{
             console.error("Erro ao marcar como lida");
@@ -90,6 +115,16 @@ function addFilter(event){
     console.log(event.currentTarget)
 
     event.currentTarget.classList.add("checked")
+
+    let filterIndex = event.currentTarget.getAttribute("data-index");
+
+    if(filterIndex == "0" ){
+        LettersRender(globalLetters, 0);
+    }else if(filterIndex == "1"){
+        LettersRender(globalLetters, 1);
+    }else{
+        LettersRender(globalLetters, 2);
+    }
 
 
 }
